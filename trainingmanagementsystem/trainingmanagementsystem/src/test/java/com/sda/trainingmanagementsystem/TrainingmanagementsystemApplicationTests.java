fcf638e6867d37a13d0d86a43d87e3350c6a6a41
Gavrilovici <igabriel1993@gmail.com>
2020-09-19 10:10:46 +0300

    My first version of final project

---
+++ trainingmanagementsystem/trainingmanagementsystem/src/test/java/com/sda/trainingmanagementsystem/TrainingmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.trainingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
